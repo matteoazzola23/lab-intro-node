58f5936f1a77bdacece2db8e64193fed34bcdbb2
teo23 <matteoazzola23@gmail.com>
2020-02-29 14:12:44 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    this.length++;
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
+ 
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } // no need to add ELSE, right?
+    return Math.max(...this.items);
+  };
 
-  max() {}
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  };
 
-  min() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");;
+    } return this.sum () / this.length;
+  }
 }
 
 module.exports = SortedList;
